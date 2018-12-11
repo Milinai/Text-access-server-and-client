11ea5cdd169eb8e013ef8bdcae3d3e04ef2e7c87
valtsu41 <null>
2018-12-11 17:46:42 +0200

    Getting (probably) closer it thanks to clang error messages!

--- server.cpp
+++ server.cpp
@@ -26,9 +26,9 @@ void Server () {
 	//This happens untill the program is closed.
 	while (true) {
 		//Listening untill something happens.
-		if (m_pServer->Listen(Connection)) {
+		if (Server->Listen(Connection)) {
 			//Sends the text to the client.
-			m_pServer->Send(Connection, Text)
+			Server->Send(Connection, Text)
 		}
 			//And if something goes wrong...
 			else {
